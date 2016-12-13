4b9d2cc33e4c48f3bfa02762f96297881cdd04d5
kazuma-sato <kazuma.sato@georgebrown.ca>
2016-12-13 15:48:33 -0500

    EnemyController Script Added
    Collider Trigger added for enemy to Player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    int damage;
+
+    void OnTriggerEnter2D(Collider2D other){
+
+        if(other.gameObject.tag == "Player") {
+            Animator playerAnimator = other.gameObject.GetComponent<Animator>();
+            if(playerAnimator.GetCurrentAnimatorStateInfo == "attack" ||
+               playerAnimator.GetCurrentAnimatorStateInfo == "jumpAtk") {
+                Destroy(gameObject);
+            } else {
+                Player.get_Instance().setHealth(Player.get_Instance().getHealth() - damage);
+            }
+        }
+    }
+    // Just in case OnTriggerEnter2D() doesn't invoke on enter.
+    void OnTriggerStay2D(Collider2D other){
+
+        OnTriggerEnter2D(other);
+    }
+
+}
